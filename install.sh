c0ac10078bb9908b6f6a548c7f45ec18e6b85beb
luckyy777 <76631426+luckyy777@users.noreply.github.com>
2022-04-29 10:41:15 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+mkdir install-dir
+cd install-dir
 git clone https://github.com/luckyy777/civ-v-drafter
 cd civ-v-drafter/
 sudo make install
-cd ../
-rm -rf civ-v-drafter/
+cd ../../
+rm -rf install-dir/
 echo "All Done :)"
 
